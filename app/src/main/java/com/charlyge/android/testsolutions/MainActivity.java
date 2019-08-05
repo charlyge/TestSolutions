8515e6712d7d1540bbbc1b5258d4a55dd9dc1c62
charlyge <charlyge2@gmail.com>
2019-08-05 08:53:02 +0100

    first commit

---
+++ app/src/main/java/com/charlyge/android/testsolutions/MainActivity.java
@@ -0,0 +1,16 @@
+package com.charlyge.android.testsolutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.charlyge.android.testsolutions.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
